2270550aaeec53eec4bc9059737d0343f707100e
Sarah <u31586@hs-harz.de>
2018-08-12 10:19:11 +0200

    nearer clipping plane, objects rotate like the working plane when instantiating, vuforia test scene, alot of example content

---
+++ Assets/Scripts/GameManage.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class GameManage : MonoBehaviour {
+
+    public List<GameObject> targetPrefabs;
+    private List<GameObject> targetList;
+
+    private int currentTargetID;
+
+    void Start () {
+
+        targetList = new List<GameObject>();
+        GameObject t;
+
+        foreach (GameObject target in targetPrefabs)
+        {
+            t = Instantiate(target);
+            //t.SetActive(false);
+            targetList.Add(t);
+        }
+
+        if(targetList.Count > 0)
+        {
+            targetList[0].SetActive(true);
+            currentTargetID = 0;
+        }
+        else
+        {
+            Debug.Log("Target List is empty");
+        }
+
+    }
+	
+	void Update () {
+		
+	}
+
+    public void ShowNext()
+    {
+
+        if (currentTargetID + 1 < targetList.Count)
+        {
+            // hide current target
+            targetList[currentTargetID].SetActive(false);
+
+            // show next target
+            targetList[currentTargetID + 1].SetActive(true);
+            currentTargetID = currentTargetID + 1;
+        }
+        else if(targetList.Count > 0)
+        {
+            // hide current target
+            targetList[currentTargetID].SetActive(false);
+
+            // show first target again
+            targetList[0].SetActive(true);
+            currentTargetID = 0;
+        }
+    }
+
+    public void PlayCamera()
+    {
+        CameraDevice.Instance.Init(CameraDevice.CameraDirection.CAMERA_DEFAULT);
+
+        CameraDevice.Instance.Start();
+    }
+
+    public void StopCamera()
+    {
+        CameraDevice.Instance.Stop();
+
+        CameraDevice.Instance.Deinit();
+    }
+}
---
+++ Assets/Scripts/OrigamiManager.cs
@@ -0,0 +1,129 @@
+﻿using HoloToolkit.Examples.InteractiveElements;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class OrigamiManager : MonoBehaviour {
+
+    public GameObject workingArea;
+    public GameObject startButton;
+    public List<GameObject> origamiStepPrefabs;
+    public float distanceToTable = 0.1f;
+
+    private GameObject workingPlane;
+    private TapToPlace tapToPlace;
+    private GameObject currentShownModel;
+    private MeshRenderer workPlaceMesh;
+
+    private Vector3 objectPosition;
+    private Quaternion cameraRotationOnPlacement;
+    private int currentModelID = 0;
+
+    private bool hasBeenPlaced = false;
+    private bool gameStarted = false;
+    private bool changedPosition = true;
+
+    void Start () {
+
+        if(workingArea != null)
+        {
+            workingArea = Instantiate(workingArea);
+        }
+
+        if(origamiStepPrefabs.Count == 0)
+        {
+            Debug.Log("No Prefabs defined");
+        }
+
+        workingPlane = workingArea.transform.Find("WorkingPlane").gameObject;
+        tapToPlace = workingPlane.GetComponent<TapToPlace>();
+
+        workPlaceMesh = workingArea.transform.Find("Cube").gameObject.GetComponent<MeshRenderer>();
+	}
+	
+	void Update () {
+
+        // placement mode
+        if (tapToPlace.IsBeingPlaced)
+        {
+            changedPosition = true;
+            workPlaceMesh.enabled = true;
+
+            if (currentShownModel != null)
+                currentShownModel.SetActive(false);
+
+            if(!gameStarted && startButton != null)
+                startButton.SetActive(false);
+        }
+        // static mode
+        else
+        {
+            workPlaceMesh.enabled = false;
+
+            if (changedPosition)
+            {
+                objectPosition = new Vector3(workingArea.transform.position.x, workingArea.transform.position.y + distanceToTable, workingArea.transform.position.z);
+                cameraRotationOnPlacement = Quaternion.Euler(0, CameraCache.Main.transform.localEulerAngles.y, 0);
+
+                if (currentShownModel != null)
+                {
+                    currentShownModel.SetActive(true);
+                    currentShownModel.transform.position = objectPosition;
+                }
+
+                if (!gameStarted)
+                {
+                    startButton.SetActive(true);
+                    startButton.transform.position = objectPosition;
+                }
+
+                changedPosition = false;
+            }
+
+            // work place is placed the first time
+            if (!hasBeenPlaced)
+            {
+                hasBeenPlaced = true;
+
+                if (startButton != null)
+                {
+                    startButton = Instantiate(startButton, objectPosition, Quaternion.identity);
+                    SetButtonEvent();
+                }
+            }
+        }	
+	}
+
+    public void ShowNext()
+    {
+        if (gameStarted)
+        {
+            if (currentModelID >= origamiStepPrefabs.Count)
+                currentModelID = 0;
+
+            if(currentShownModel!= null)
+                Destroy(currentShownModel);
+
+            currentShownModel = Instantiate(origamiStepPrefabs[currentModelID], objectPosition, cameraRotationOnPlacement);
+            currentModelID++;
+        }
+    }
+
+    public void StartGame()
+    {
+        gameStarted = true;
+        startButton.SetActive(false);
+        ShowNext();
+    }
+
+    private void SetButtonEvent()
+    {
+        UnityEvent e = new UnityEvent();
+        e.AddListener(StartGame);
+        startButton.transform.Find("Button").gameObject.GetComponent<InteractiveButton>().OnSelectEvents = e;
+    }
+}
